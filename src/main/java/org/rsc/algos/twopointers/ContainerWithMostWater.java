4aec080909a72a2eb9d8d87298758f7cab203aad
RSChaubal <rushikesh.chaubal@gmail.com>
2025-08-19 00:28:06 +0530

    Leet code submission

---
+++ src/main/java/org/rsc/algos/twopointers/ContainerWithMostWater.java
@@ -0,0 +1,64 @@
+package org.rsc.algos.twopointers;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ *
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ *
+ * Return the maximum amount of water a container can store.
+ *
+ * Notice that you may not slant the container.
+ *
+ *
+ * Example 1:
+ *
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+ * In this case, the max area of water (blue section) the container can contain is 49.
+ * Example 2:
+ *
+ * Input: height = [1,1]
+ * Output: 1
+ *
+ *
+ * Constraints:
+ *
+ * n == height.length
+ * 2 <= n <= 105
+ * 0 <= height[i] <= 104
+ *
+ */
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+
+        int [] height = {1,8,6,2,5,4,8,3,7} ;
+        System.out.println("Max Area for array " + Arrays.toString(height) + " is: " + maxArea(height)) ;
+
+        int [] height1 = {1,1} ;
+        System.out.println("Max Area for array " + Arrays.toString(height1) + " is: " + maxArea(height1)) ;
+    }
+
+    public static int maxArea(int[] height) {
+        int maxArea = 0 ;
+
+        int i = 0, j = height.length - 1 ;
+        while (i < j) {
+            int currentArea = Math.min(height[i], height[j]) * (j-i) ;
+            maxArea = Math.max(maxArea, currentArea) ;
+            if (height[i] < height[j]) {
+                i++ ;
+            }
+            else {
+                j-- ;
+            }
+        }
+        return maxArea ;
+    }
+
+}
